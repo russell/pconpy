ebf8b2922c42cb0194c77b2b72427187f8519848
Russell Sim <russell.sim@gmail.com>
2012-12-12 22:15:59 +1100

    initial setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import sys, os
+
+
+setup(name='pconpy',
+      version=0.1,
+      description="",
+      long_description="",
+      classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Topic :: System :: Distributed Computing",
+        "Intended Audience :: End Users/Desktop",
+        "Environment :: Console",
+        "Topic :: Security",
+        ],
+      keywords='',
+      author='Russell Sim',
+      author_email='russell.sim@gmail.com',
+      url='',
+      download_url='',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      """,
+      )
